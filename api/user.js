bb00677f14d6c4b91fcb9bffd4eda760d5aa2d9a
Bjarte Larsen <bjartekll555@gmail.com>
2016-11-17 03:46:55 +0100

    added create user

--- api/user.js
+++ api/user.js
@@ -2,7 +2,8 @@
 
 let bluebird  = require('bluebird'),
     User = require('../models/User.js'),
-    jwt = require('jsonwebtoken');
+    jwt = require('jsonwebtoken'),
+    hash = require('hash.js')
 
 
 module.exports = (api) => {
@@ -10,6 +11,35 @@ module.exports = (api) => {
   api.route('/user/:id')
     .get((req, res) => {
       res.send("Get a user!")
+    })
+  api.route('/user')
+    .post((req, res) => {
+      User.getUser({nick: req.body.nick}, function(err, data) {
+        if(data.length) {
+          return res.send({
+            error: true,
+            error_msg: "Nickname in use",
+          })
+        }
+        User.getUser({number: req.body.number}, function(err, data) {
+          if(data.length)
+            return res.send({
+              error: true,
+              error_msg: "Phone number in use",
+            })
+
+          User.createUser({
+            number: req.body.number,
+            nick: req.body.nick,
+            password: hash.sha256().update(req.data.password).digest('hex');
+          }, function(err, data) {
+            return res.send({
+              status: 200,
+              msg: "User created!"
+            })
+          })
+        })
+      })
     })
 
   api.route('/user/auth')
@@ -25,8 +55,7 @@ module.exports = (api) => {
         password: req.body.password,
       }, function(err, data) {
         if(err || !data.length) {
-
-          res.send({error: true, err});
+          res.send({error: true});
         }else {
           let token = jwt.sign({ uid: data[0]._id }, 'supersecret');
           res.send({
