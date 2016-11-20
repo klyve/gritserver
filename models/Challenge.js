8f1d29a19bcd80d6215cfeef541646bfce68d143
Arxcis_MBP <jonas.solsvik@gmail.com>
2016-11-20 02:52:28 +0100

    Challange.js #28 created ChallengeSchema

--- models/Challenge.js
+++ models/Challenge.js
@@ -2,4 +2,30 @@
 let mongoose = require('mongoose');
 
 
-var GroupSchema = 
+var ChallengeSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+    creator: {
+        type: String,
+        required: true,
+    },
+    images: { 
+        type: Array,       // valid url
+        default: [],
+    },
+    description: {
+        type: String,
+        maxlength: 3000,
+    },
+    endtime: {
+        type: Date,
+        required: true,
+        default: ( Date.now() / 1000 * 3600 * 24 ),
+    },
+
+});
+
+let ChallangeModel = module.exports = mongoose.model('ChallangeModel', ChallangeSchema, 'challanges');
