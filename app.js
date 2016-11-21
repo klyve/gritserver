5d7a681abf587c62f4cd7cda8e5b3fc1483a4837
Bjarte Larsen <bjartekll555@gmail.com>
2016-11-21 03:27:50 +0100

    prepared for server release

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.all('*', (req,res) => {                           // Sets up an error messag
     "error_message": "Bad request, 404 not found"
   })
 })
-app.listen(3000, function () {
+app.listen(80, function () {
   console.log('app listening on port 80!')
 })
 
